249569a94b7739b8a47df014f092b4151ca825f7
j0nathanb <j0nathanb.dev@gmail.com>
2025-07-14 15:26:30 -0700

    Initial Commit

---
+++ amp/ampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ampApp.swift
+//  amp
+//
+//  Created by zen on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ampTests/ampTests.swift
@@ -0,0 +1,17 @@
+//
+//  ampTests.swift
+//  ampTests
+//
+//  Created by zen on 7/14/25.
+//
+
+import Testing
+@testable import amp
+
+struct ampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
